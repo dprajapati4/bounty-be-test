4afd6c8bb67292a91fc19342b00ffb91df32b17d
Deep Prajapati <dprajap2@gmail.com>
2022-06-14 12:18:54 -0400

    add axios and fn to remove duplicates

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,71 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const axios_1 = __importDefault(require("axios"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server is running');
-});
+app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.urlencoded({
     extended: true
 }));
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server is running');
+});
+app.get('/api', (req, res) => {
+    res.send('on api route');
+});
+const fetchData = (query) => __awaiter(void 0, void 0, void 0, function* () {
+    const { data } = yield axios_1.default.get(`https://pokeapi.co/api/v2/pokemon/${query}`);
+    if (data) {
+        return data;
+    }
+});
+const removeDuplicates = (query) => {
+    const queryArr = query.split(',');
+    console.log('remove', queryArr);
+    const uniqueQueries = [];
+    queryArr.forEach((query) => {
+        if (!uniqueQueries.includes(query)) {
+            uniqueQueries.push(query);
+        }
+        console.log('sfter', uniqueQueries);
+    });
+    return uniqueQueries;
+};
+//https://pokeapi.co/api/v2/pokemon/{id or name}/
+app.get('/pokemon-details', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const pokemon = req.query.pokemon;
+    console.log('pokemon', removeDuplicates(pokemon));
+    try {
+        const data = yield fetchData(pokemon);
+        res.send(data);
+    }
+    catch (error) {
+        console.log('Error fetching pokemon data', error);
+    }
+}));
+// Error handling endware
+app.use((err, req, res, next) => {
+    console.error('Endware erroree', err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error');
+});
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- index.ts
+++ index.ts
@@ -1,18 +1,69 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is running');
 });
 
-app.use(express.urlencoded({
-  extended:true
-}));
+
+app.get('/api', (req: Request, res: Response) => {
+  res.send('on api route');
+});
+
+const fetchData = async (query: any) => {
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${query}`)
+  if (data) {
+    return data
+  }
+};
+
+const removeDuplicates = (query: any) => {
+  const queryArr: Array<string> = query.split(',')
+  console.log('remove', queryArr)
+  const uniqueQueries: any = [];
+  queryArr.forEach((query: any) => {
+    if (!uniqueQueries.includes(query)) {
+      uniqueQueries.push(query);
+    }
+    console.log('sfter', uniqueQueries)
+  });
+  return uniqueQueries
+}
+
+//https://pokeapi.co/api/v2/pokemon/{id or name}/
+app.get('/pokemon-details', async (req: Request, res: Response, next: NextFunction) => {
+  const pokemon = req.query.pokemon
+  console.log('pokemon', removeDuplicates(pokemon))
+  try {
+    const data = await fetchData(pokemon)
+    res.send(data)
+  } catch (error) {
+    console.log('Error fetching pokemon data', error)
+  }
+
+});
+
+// Error handling endware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error('Endware erroree', err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
